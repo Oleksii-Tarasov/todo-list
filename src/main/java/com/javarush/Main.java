362e37c8362af5e9786d3153624ef90829e92a48
Oleksii-Tarasov <alekseytarasov85@gmail.com>
2023-03-15 10:22:02 +0200

    Created project structure.

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -1,2 +1,7 @@
-package com.javarush;public class Main {
+package com.javarush;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("We start");
+    }
 }

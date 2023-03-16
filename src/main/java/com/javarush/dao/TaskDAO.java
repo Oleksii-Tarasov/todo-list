f2fb0375b81c3c1911f4ae930b7f31159b6f3480
Oleksii-Tarasov <alekseytarasov85@gmail.com>
2023-03-16 15:02:22 +0200

    Realised TaskDAO.

--- src/main/java/com/javarush/dao/TaskDAO.java
+++ src/main/java/com/javarush/dao/TaskDAO.java
@@ -1,4 +1,59 @@
 package com.javarush.dao;
 
+import com.javarush.domain.Task;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
 public class TaskDAO {
+
+    private final SessionFactory sessionFactory;
+
+    public TaskDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    public int getAllCount() {
+        Query<Long> query = getSession().createQuery("SELECT COUNT(t) FROM Task t", Long.class);
+
+        return Math.toIntExact(query.getSingleResult());
+    }
+
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    public List<Task> getAll(int offset, int limit) {
+        Query<Task> query = getSession().createQuery("SELECT t FROM Task t", Task.class);
+        query.setFirstResult(offset);
+        query.setMaxResults(limit);
+
+        return query.getResultList();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Task getById(int id) {
+        Query<Task> query = getSession().createQuery("SELECT t FROM Task t WHERE t.id =: id", Task.class);
+        query.setParameter("id", id);
+
+        return query.getSingleResult();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void edit(Task task) {
+        getSession().persist(task);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void delete(Task task) {
+        getSession().remove(task);
+    }
+
+    private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
 }
